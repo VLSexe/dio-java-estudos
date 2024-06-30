b8c711ab009fcf7226990ce48cffbee327a49b46
Victor Lopes dos Santos <106280251+VLSexe@users.noreply.github.com>
2024-06-30 15:56:10 -0300

    add unarios example

---
+++ src/edu/victor/java/operadores/Unarios.java
@@ -0,0 +1,26 @@
+package edu.victor.java.operadores;
+
+public class Unarios {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        //Imprimindo o numero negativo
+        System.out.println(- numero);
+
+        //incrementando numero em mais 1 numero, imprime 6
+        numero ++;
+        System.out.println(numero);
+
+        //incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero ++);// ops algo de errado não está certo
+
+        System.out.println(numero);// agora sim, numero virou 7
+
+        //ordem de precedencia conta aqui
+        System.out.println(++ numero);
+
+        boolean verdadeiro = true;
+
+        System.out.println("Inverteu " + !verdadeiro);
+    }
+}
