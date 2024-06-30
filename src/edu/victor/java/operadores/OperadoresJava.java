1e0c51186b24f4322859332b78863e5ebd393e56
Victor Lopes dos Santos <106280251+VLSexe@users.noreply.github.com>
2024-06-30 14:45:12 -0300

    Update struct project

---
+++ src/edu/victor/java/operadores/OperadoresJava.java
@@ -0,0 +1,11 @@
+package edu.victor.java.operadores;
+
+public class OperadoresJava {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
